7372cdaddbd53ac122b90383326fc219490614e3
Jakob Laback <jlaback@student.tgm.ac.at>
2018-10-20 22:28:00 +0200

    Finished

--- src/Panel.java
+++ src/Panel.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.*;
 
@@ -15,6 +16,9 @@ public class Panel extends JPanel{
 	
 	protected void paintComponent (Graphics g) {
 		super.paintComponent(g);
+		g.setColor(Color.BLUE);
 		
+		g.drawString("Der Verschlüsselte Text: "+"\t" + this.m1.getencryptedText(), 20, 40 );
+		g.drawString("Der Entschlüsselte Text: "+"tn" + this.m1.getdecryptedText(), 20, 50);
 	}
 }
